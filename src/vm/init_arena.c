47bb16e0a70ec13ee76b3618c505ec9140d2342f
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-10-14 17:54:35 +0300

    Start of init_arena

--- src/vm/init_arena.c
+++ src/vm/init_arena.c
@@ -6,13 +6,26 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:28:19 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/10/14 16:29:14 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/10/14 17:39:01 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
+static void	put_champs(t_mngr *mngr)
+{
+	int i;
+
+	i = -1;
+	while (++i < mngr->chmp_num)
+		ft_memcpy(mngr->arena + i * MEM_SIZE / mngr->chmp_num,
+				mngr->chmps[i].code, mngr->chmps[i].size);
+}
+
 void	init_arena(t_mngr *mngr)
 {
-	
+	if (!(mngr->arena = ft_memalloc(MEM_SIZE)))
+		safe_exit(mngr, MALLOC_ERROR);
+	put_champs(mngr);
+
 }
