f47f1263ee7f36b7cd1cd117b39ef3137d0b235f
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-10-15 23:35:36 +0300

    Layout game loop
    Took 3 hours 44 minutes

--- src/vm/game_main_loop.c
+++ src/vm/game_main_loop.c
@@ -6,13 +6,31 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:26:51 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/10/14 16:27:39 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/10/15 17:03:54 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	game_main(t_mngr *mngr)
+void	check_cars(t_mngr *mngr)
 {
 
+}
+
+void	dump_arena(t_mngr *mngr)
+{
+
+}
+
+void	game_main(t_mngr *mngr)
+{
+	while (mngr->num_cars)
+	{
+		if (mngr->cycle >= mngr->cycles_to_die || mngr->cycles_to_die <= 0)
+			check_cars(mngr);
+		make_one_turn(mngr);
+		if (mngr->flags & DUMP)
+			dump_arena(mngr);
+		mngr->cycle++;
+	}
 }
