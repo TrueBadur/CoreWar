8113e55f90d8a742330e8591a137422ece2386ee
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-10-17 18:14:16 +0300

    Finish game_loop with proceed_cars. Still need testing.
    Took 31 minutes

--- src/vm/make_one_turn.c
+++ src/vm/make_one_turn.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 17:06:37 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/10/16 23:40:40 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/10/17 17:51:34 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,34 @@ void	handle_op(t_mngr *mngr, t_car *car)
 	op = (t_t_op){BYTE_OP, ARG1(BYTE_CODE), ARG2(BYTE_CODE), ARG3(BYTE_CODE)};
 	if ((ret = check_op(&op)) > 0)
 		get_op_func(op.op)(mngr, car);
-	car->byte_next_op = FT_ABS(ret);
+	car->pos = (car->pos + FT_ABS(ret)) % MEM_SIZE;
 }
 
-void	proceed_cars(t_mngr *mngr, t_car *car)
+void proceed_cars(t_mngr *mngr, short cur_time)
 {
+	t_list_node *lst;
+	t_car		*car;
+	char		op;
+	int			time_to_put;
+
+	while ((lst = ft_lsttake(mngr->timeline[cur_time])))
+	{
+		car = (t_car*)lst->content;
+		op = (char)mngr->arena[car->pos];
+		if (OP_live <= op && OP_aff >= op)
+			time_to_put = cur_time + get_op_info(op)->num_of_ticks + 1;
+		else
+			time_to_put = cur_time + 1;
+		ft_lstadd(mngr->timeline[time_to_put % (MAX_OP_TIME + 1)],lst);
+	}
 }
-
-
 
 void	make_one_turn(t_mngr *mngr)
 {
+	short	cur_time;
+
 	tl_car_iter(mngr, handle_op);
-	tl_car_iter(mngr, proceed_cars);
-	//for every car in cur time
-	//	if op under car is valid and args for op is valid
-	//			execute op
-	//for every car in cur time
-	//	move car to new pos
-	//	read op and move car on timeline
+	cur_time = (short)(mngr->cycle % (MAX_OP_TIME + 1));
+	proceed_cars(mngr, cur_time);
 }
 
