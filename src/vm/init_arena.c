bca23aee46981480444835429e0fd0838766cf76
Yuriy <tonyaevv@mail.ru>
2019-11-13 17:28:03 +0300

    Fix champ wrong id setting

--- src/vm/init_arena.c
+++ src/vm/init_arena.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_arena.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: blomo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:28:19 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/11/12 19:48:08 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/11/13 17:17:43 by blomo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,27 @@ static void init_carrieges(t_mngr *mngr)
 {
 	int i;
 	t_car	*car;
+	int j;
 
 	if (!(mngr->cars = ft_vecinit(2 * mngr->chmp_num)))
 		safe_exit(mngr, MALLOC_ERROR);
-	i = -1;
-	while (++i < mngr->chmp_num)
+	i = 0;
+	j = -1;
+	while (++j < MAX_PLAYERS)
 	{
+		if (!mngr->chmps[j])
+			continue ;
 		if (!(car = ft_memalloc(sizeof(t_car))))
 			safe_exit(mngr, MALLOC_ERROR);
 		car->id = mngr->next_id++;
-		car->pos = i * MEM_SIZE / mngr->chmp_num;
+		car->pos = i++ * MEM_SIZE / mngr->chmp_num;
 		car->eval_in = 1;
 		if (!(mngr->cars = ft_vecpush(mngr->cars, &car, sizeof(void *))))
 			safe_exit(mngr, MALLOC_ERROR);
 		tl_put(mngr, 1, car);
 		mngr->num_cars++;
-		*(int*)car->regs = (int)(-car->id - 1);
-		mngr->winner = car->id;
+		*(int*)car->regs = (int)(-j - 1);
+		mngr->winner = j;
 	}
 }
 
--- src/vm/validate_input.c
+++ src/vm/validate_input.c
@@ -6,12 +6,12 @@
 /*   By: blomo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:39:49 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/11/13 16:41:24 by blomo            ###   ########.fr       */
+/*   Updated: 2019/11/13 17:11:37 by blomo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
-#include "limits.h"
+
 
 int handle_n(t_mngr *mngr, char **argv, int i)
 {
@@ -107,40 +107,11 @@ void check_players(t_mngr *mngr, char **argv, int argc)
         }
     }
 }
-void sort_champ(t_mngr *mngr)
-{
-	int i;
-	int k;
-	t_chmp *temp[4];
-
-	k = 0;
-	i = -1;
-	while(++i < 4)
-	{
-		if (mngr->chmps[i])
-		{
-			temp[k] = (t_chmp*)malloc(sizeof(t_chmp));
-			ft_memcpy(temp[k],mngr->chmps[i], sizeof(t_chmp));
-			free(mngr->chmps[i]);
-			mngr->chmps[i] = NULL;
-			k++;
-		}
-	}
-	while(--k > -1)
-	{
-		mngr->chmps[k] = (t_chmp*)malloc(sizeof(t_chmp));
-		ft_memcpy(mngr->chmps[k],temp[k], sizeof(t_chmp));
-		ft_bzero(temp[k], sizeof(t_chmp));
-		free(temp[k]);
-	}
-}
 
 void validate_input(t_mngr *mngr, int argc, char **argv)
 {
-    if(argc == 1)
+    if (argc == 1)
         safe_exit(mngr, FEW_ARGUMENTS);
 	parse_flags(mngr, argv);
     check_players(mngr,argv,argc);
-    if (mngr->flags & FLAG_N)
-    	sort_champ(mngr);
 }
