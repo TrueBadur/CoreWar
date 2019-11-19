ad353cc615a0833b0c3134a8d9e772b5d1f17fae
wgorold <philipp.nox@gmail.com>
2019-11-20 00:45:32 +0300

    WIP: merge

---
+++ src/sort/merge.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blomo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 16:24:31 by ehugh-be          #+#    #+#             */
+/*   Updated: 2019/11/13 20:11:41 by blomo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+#include "visu.h"
+
+static t_mngr	*init_mngr(void)
+{
+	t_mngr		*mngr;
+
+	if (!(mngr = ft_memalloc(sizeof(t_mngr))))
+		safe_exit(mngr, MALLOC_ERROR, NULL);
+	ft_bzero(mngr->timeline, (MAX_OP_TIME + 1) * sizeof(void*));
+	return (mngr);
+}
+
+static void		champion_introduce(t_mngr *mngr)
+{
+	int			i;
+
+	i = -1;
+	if (mngr->flags & FLAG_S)
+		return ;
+	ft_printf("Introducing contestants...\n");
+	while (++i < 4)
+	{
+		if (mngr->chmps[i])
+			ft_printf("* Player %d, weighing %d bytes, \"%s\" (\"%s\") !\n",
+			i + 1, mngr->chmps[i]->size, mngr->chmps[i]->name,
+			mngr->chmps[i]->moto);
+	}
+}
+
+static void		winer_anounce(t_mngr *mngr)
+{
+	show_winner(mngr);
+	if (!(mngr->flags & FLAG_S))
+		ft_printf("Contestant %d, \"%s\", has won !\n", mngr->winner + 1,
+	mngr->chmps[mngr->winner]->name);
+}
+
+int				main(int argc, char **argv)
+{
+	t_mngr		*mngr;
+
+	mngr = init_mngr();
+	validate_input(mngr, argc, argv);
+	start_visu(mngr);
+	init_arena(mngr);
+	champion_introduce(mngr);
+	game_main(mngr);
+	winer_anounce(mngr);
+	safe_exit(mngr, SUCCESS, NULL);
+	endwin();
+	return (0);
+}
